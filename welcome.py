6b9a1dc06917b6ad1502103c6975bd4d42a4f4b3
flutter-lord <richardolojede2@gmail.com>
2025-10-04 03:11:59 +0100

    git commit -m "commmit"

---
+++ welcome.py
@@ -0,0 +1,40 @@
+import turtle
+
+print("hello the name of my wife is Richard")
+
+print((10.5 + 2 * 3)/ (45 - 3.5))
+   
+turtle.write("Welcome to python Richard")
+
+turtle.color("yellow")
+turtle.forward(100)
+
+#turtle.right(90)
+
+turtle.color("red")
+turtle.forward(100)
+
+turtle.right(90)
+
+turtle.color("blue")
+turtle.forward(130)
+
+turtle.color("black")
+turtle.right(60)
+turtle.forward(90)
+
+turtle.penup()
+turtle.goto(-50, 50)
+turtle.pendown()
+turtle.goto(20, 100)
+turtle.goto(10, 50)
+turtle.color("red")
+turtle.circle(100)
+turtle.color('red')
+turtle.circle(50)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.backward(100)
+
+turtle.exitonclick()
