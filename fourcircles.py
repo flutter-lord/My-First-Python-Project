e2164b3bbf3dd072a33373e8c766a1d4184e1504
flutter-lord <richardolojede2@gmail.com>
2025-10-08 23:08:37 +0100

    git commit -m "commmit"

---
+++ fourcircles.py
@@ -0,0 +1,28 @@
+import turtle
+t = turtle
+
+t.penup()
+t.color("red")
+t.goto(-100,20)
+t.pendown()
+t.circle(45)
+
+t.penup()
+t.color("blue")
+t.goto(-9, 20)
+t.pendown()
+t.circle(45)
+
+t.penup()
+t.color("black")
+t.goto(-100, -70)
+t.pendown()
+t.circle(45)
+
+t.penup()
+t.color("yellow")
+t.goto(-9, -70)
+t.pendown()
+t.circle(45)
+
+t.done()
