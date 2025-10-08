3e16d9a1b4a434abbae215b37c4822e55000c31a
flutter-lord <richardolojede2@gmail.com>
2025-10-08 06:03:31 +0100

    git commit -m "commmit"

--- triangle1.py
+++ triangle1.py
@@ -1,16 +1,22 @@
 import turtle
 t = turtle
 
-t.right(60)
+'''t.right(45)
 t.forward(100)
+#t.left(225) so in this case right (135) is the same as left(225)
+t.right(135)
+t.forward(140)
+t.right(135)
+t.forward(100)'''
 
-t.right(180)
+t.penup()
+t.goto(0,40)
+t.pendown()
+t.left(225)
 t.forward(100)
-
-t.right(60)
-t.forward(100)
-
-t.right(60)
+t.left(135)
+t.forward(140)
+t.left(135)
 t.forward(100)
 
 t.done()
