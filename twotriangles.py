d016960fbd560405878746ddb8bc9f9a78734895
flutter-lord <richardolojede2@gmail.com>
2025-10-08 06:03:19 +0100

    git commit -m "commmit"

---
+++ twotriangles.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle
+
+t.right(45)
+t.forward(100)
+t.right(135)
+t.forward(130)
+t.right(135)
+t.forward(183)
+t.left(135)
+t.forward(130)
+t.left(135)
+t.forward(100)
+
+t.done()
