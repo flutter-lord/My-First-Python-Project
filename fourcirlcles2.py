475b2d68ee89a613a193740c4ab6573cb4681288
flutter-lord <richardolojede2@gmail.com>
2025-10-10 12:27:59 +0100

    commit

--- fourcirlcles2.py
+++ fourcirlcles2.py
@@ -1,8 +1,9 @@
 import turtle
 
+t =turtle
+
 circleRadius = eval(input("please, Enter the radius of the circle : "))
 
-t =turtle
 
 t.penup()
 t.goto(-69, 00)
@@ -23,6 +24,27 @@ t.penup()
 t.goto(-69, -99)
 t.pendown()
 t.circle(circleRadius)
+
+t.penup()
+t.goto(-100, 100)
+t.pendown()
+t.forward(180)
+
+t.right(90)
+t.forward(200)
+
+t.right(90)
+t.forward(180)
+
+t.right(90)
+t.forward(200)
+
+
+
+
+
+
+
 
 t.done()
 
