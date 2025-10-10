ff005aec6b5f1b0cea6dbe9085336b8853d8ab5b
flutter-lord <richardolojede2@gmail.com>
2025-10-10 12:18:22 +0100

    commit

--- fourcirlcles2.py
+++ fourcirlcles2.py
@@ -1,9 +1,30 @@
-monthlySavings, annualInterestRate = eval(input("Enter the amount you saved every months"
-"and the annual interest rate : "))
+import turtle
 
-monthlyInterestRates = annualInterestRate / 100 / 12
+circleRadius = eval(input("please, Enter the radius of the circle : "))
 
-initialInterest = 0
+t =turtle
+
+t.penup()
+t.goto(-69, 00)
+t.pendown()
+t.circle(circleRadius)
+
+t.penup()
+t.goto(30, 0)
+t.pendown()
+t.circle(circleRadius)
+
+t.penup()
+t.goto(30, -99)
+t.pendown()
+t.circle(circleRadius)
+
+t.penup()
+t.goto(-69, -99)
+t.pendown()
+t.circle(circleRadius)
+
+t.done()
 
 
 
