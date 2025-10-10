c6e8233b0423ad506269a5056aaa5b1fbb2ea0c5
flutter-lord <richardolojede2@gmail.com>
2025-10-10 11:37:43 +0100

    commit

---
+++ fourcirlcles2.py
@@ -0,0 +1,12 @@
+monthlySavings, annualInterestRate = eval(input("Enter the amount you saved every months"
+"and the annual interest rate : "))
+
+monthlyInterestRates = annualInterestRate / 100 / 12
+
+initialInterest = 0
+
+
+
+
+
+
