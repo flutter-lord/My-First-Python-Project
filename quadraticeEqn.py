bff096447cb72b607c345d875080dc86cc689f54
flutter-lord <richardolojede2@gmail.com>
2025-10-12 17:40:45 +0100

    commit

--- quadraticeEqn.py
+++ quadraticeEqn.py
@@ -3,5 +3,5 @@ a, b, c = eval(input("Enter your value for A, B and C : "))
 X1 = (-b + ((b ** 2) - (4 * a * c)) ** 0.5) / 2 * a
 
 X2 = (-b - ((b ** 2) - (4 * a * c)) ** 0.5) / 2 * a
-print("The roots for the equation", a,"X^2 +", b,"X","+", c, "are", X1, "and", X2 )
+print("The roots for the equatgggggggion", a,"X^2 +", b,"X","+", c, "are", X1, "and", X2 )
 
