fc43fa09c937b43106b8dc18b6ef412e898fbe3b
flutter-lord <richardolojede2@gmail.com>
2025-10-13 22:49:45 +0100

    commit

--- example.py
+++ example.py
@@ -4,4 +4,6 @@ print(ord("A"))
 
 print(chr(98))
 
-print("This is my mum\'s isalsomnffgjklgfThe name of my\nschool i funaab")
+print("This is my mum\'s isalsomnffgjklgfThe name of mygfd\nschool i funaab")
+
+print("He said, \"John program is very easy to read\"")
