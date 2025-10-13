04f34c31a8da2aaf7d816d8335e8eb89afeb9054
flutter-lord <richardolojede2@gmail.com>
2025-10-13 22:59:24 +0100

    commit

--- example.py
+++ example.py
@@ -7,3 +7,11 @@ print(chr(98))
 print("This is my mum\'s isalsomnffgjklgfThe name of mygfd\nschool i funaab")
 
 print("He said, \"John program is very easy to read\"")
+
+print("bayo", end = " lanre")
+
+print(" olojede", end = " Richard\n")
+
+print("the area of the circle is 13", end = ' ')
+
+print("the area of the perimeter is 6")
