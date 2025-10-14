6a30fac4d63a4c6db909f590f520a35c88482df0
flutter-lord <richardolojede2@gmail.com>
2025-10-14 22:53:34 +0100

    commit

--- dollar_conversion.py
+++ dollar_conversion.py
@@ -4,19 +4,19 @@ cent = dollars * 100
 
 new_dollars = int(cent // 100)
 
-cent = cent % 100 #cent - (new_dollars * 100)
+cent -= (new_dollars * 100)
 
 quarters = int(cent // 25)
 
-cent = cent % 25 #cent - (quarters * 25)
+cent -= (quarters * 25)
 
 dime = int(cent // 10)
 
-cent = cent % 10 #cent - (dime * 10)
+cent -= (dime * 10)
 
 nickel = int(cent // 5)
 
-cent = cent % 5 #cent - (nickel * 5)
+cent -= (nickel * 5)
 
 penny = int(cent)
 
