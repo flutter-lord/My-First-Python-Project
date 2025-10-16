c87c092d7140160b83e35f878f6d56435cdba582
flutter-lord <richardolojede2@gmail.com>
2025-10-16 14:35:27 +0100

    commit

---
+++ human_face.py
@@ -0,0 +1,38 @@
+import turtle as t
+
+t.speed(1)
+t.penup()
+t.sety(-120)
+t.pendown()
+t.circle(120)
+
+t.penup()
+t.goto(-75, -40)
+t.pendown()
+
+t.right(30)
+t.forward(80)
+t.left(60)
+t.forward(80)
+
+
+
+t.penup()
+t.goto(30, -40)
+t.pendown()
+t.left(90)
+t.forward(70)
+t.left(120)
+t.forward(70)
+
+t.penup()
+t.goto(-60, 50)
+t.pendown()
+t.dot(25)
+
+t.penup()
+t.setx(70)
+t.pendown()
+t.dot(25)
+
+t.done()
