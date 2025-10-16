93c5687bbd2b0e9f001dd0aa286c6bb9bef1fc98
flutter-lord <richardolojede2@gmail.com>
2025-10-16 12:10:42 +0100

    commit

---
+++ hexagon_stop.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+t.hideturtle()
+t.begin_fill()
+t.circle(100,steps= 6)
+t.end_fill()
+
+t.penup()
+t.goto(-70, 70)
+t.pendown()
+t.color("white")
+t.write("STOP", font= ("Arial", 40, "bold"))
+
+
+t.done()
