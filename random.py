87e7a4984e3bfb6486de5e802eb3f07ead432c30
flutter-lord <richardolojede2@gmail.com>
2025-10-16 13:05:43 +0100

    commit

---
+++ random.py
@@ -0,0 +1,8 @@
+import turtle as t
+t.goto(100, 50)
+
+t.setx(-100)
+
+t.sety(0)
+
+t.done()
---
+++ turtle_bigger_circles.py
@@ -0,0 +1,30 @@
+import turtle as t
+
+#t.speed(5)
+t.pensize(10)
+t.penup()
+t.goto(-150, -10)
+t.pendown()
+t.circle(60)
+
+t.penup()
+t.setx(0)
+t.pendown()
+t.circle(60)
+
+t.penup()
+t.setx(150)
+t.pendown()
+t.circle(60)
+
+t.penup()
+t.goto(70, -60)
+t.pendown()
+t.circle(60)
+
+t.penup()
+t.setx(-70)
+t.pendown()
+t.circle(60)
+
+t.done()
