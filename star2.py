051fb6b12834a4bbf8c7c8a271a8759957bb518d
flutter-lord <richardolojede2@gmail.com>
2025-10-16 11:33:50 +0100

    commit

---
+++ star2.py
@@ -0,0 +1,19 @@
+import  turtle as t
+
+t.penup()
+t.pensize(2)
+t.sety(50)
+t.pendown()
+t.right(72)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+
+
+t.done()
