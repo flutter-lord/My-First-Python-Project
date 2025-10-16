67a57e8573d2de2f18d492f49807c07899a6332d
flutter-lord <richardolojede2@gmail.com>
2025-10-16 09:15:22 +0100

    commit

---
+++ object_intro_examples2.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+t.hideturtle()
+t.color("blue")
+t.begin_fill()
+t.circle(70)
+t.showturtle()
+t.fillcolor("red")
+t.end_fill()
+t.filling()
+t.screensize(10, 3300)
+
+#t.clear()
+#t.reset()
+t.done()
